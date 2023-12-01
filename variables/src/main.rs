baae0e07cb6b5247b293e0b81b5b9465b2f44af8
ivanexDev <ivanexdev@gmail.com>
2023-12-01 15:48:03 -0300

    3.1 Variables y mutabilidad

---
+++ variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
+// let -> es una variable "constnate"
+
+// let mut -> es una variable mutable
+
+// const -> es un valor que se mantiene 
+// constante y debe ser anotado(tener un tipo)
+// no se pueden utilizar para almacenar un resultado
+//en tiempo de ejecución
